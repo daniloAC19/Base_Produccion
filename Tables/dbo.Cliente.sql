3eea7272e8a6b9a5dfd3cc94228ab227e58a69ca
DaniloAC19 <server19>
2018-11-24 18:56:46 +0100

    Base de datos original

---
+++ Tables/dbo.Cliente.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Cliente]
+(
+[Codigo] [nchar] (10) COLLATE Modern_Spanish_CI_AS NULL,
+[Nombre] [char] (30) COLLATE Modern_Spanish_CI_AS NULL,
+[Estado] [char] (10) COLLATE Modern_Spanish_CI_AS NULL,
+[Pedidos] [int] NULL
+) ON [PRIMARY]
+GO
